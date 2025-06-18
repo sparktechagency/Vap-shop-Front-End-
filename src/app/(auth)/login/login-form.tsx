3cb10c0409d99cd812db71ebb540f27a2d18bdb8
Mahedihasanimam <mdmehedihasen678@gmail.com>
2025-06-18 07:39:42 +0600

    login integrated

--- src/app/(auth)/login/login-form.tsx
+++ src/app/(auth)/login/login-form.tsx
@@ -83,7 +83,7 @@ export function LoginForm({
             </div>
             <div className="!mt-4 text-center text-sm">
               Don&apos;t have an account?{" "}
-              <Link href="#" className="underline underline-offset-4">
+              <Link href="/create-an-account" className="underline underline-offset-4">
                 Sign up
               </Link>
             </div>
