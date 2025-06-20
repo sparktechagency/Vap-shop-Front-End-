ee9d349af1a077994439cfd1c352f13c07059c54
Mahedihasanimam <mdmehedihasen678@gmail.com>
2025-06-20 09:48:51 +0600

    nothing

--- src/redux/baseApi.tsx
+++ src/redux/baseApi.tsx
@@ -5,7 +5,7 @@ import Cookies from "js-cookie";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://10.0.80.13/api/",
+    baseUrl: "http://10.0.80.13/api/",
     prepareHeaders: (headers) => {
       const token = Cookies.get("token");
       console.log("tokenFromBaseApi", token);
@@ -22,4 +22,4 @@ export const api = createApi({
   endpoints: () => ({}),
 });
 
-export const imageUrl = "https://10.0.80.13";
+export const imageUrl = "http://10.0.80.13";
