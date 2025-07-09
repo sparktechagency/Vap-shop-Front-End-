0a1cdcda0208a2321b4aa09cf2b3f05d14fe176d
Mahedihasanimam <mdmehedihasen678@gmail.com>
2025-07-09 17:46:34 +0600

    updated fixed code

--- src/app/(user)/me/manage/[id]/page.tsx
+++ src/app/(user)/me/manage/[id]/page.tsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { Suspense } from "react";
 import EditForm from "./edit-form";
 
-// ✅ Async function প্রয়োজন কারণ EditForm async component expect করে
-export default async function Page({ params }: { params: { id: string } }) {
+type SegmentParams = { id: string };
+
+interface PageProps {
+  params: Promise<SegmentParams>;
+}
+
+export default async function Page({ params }: PageProps) {
+  const resolvedParams = await params; // await here because it's a Promise
+
   return (
     <div className="!py-12">
       <h2 className="mb-6! font-semibold text-3xl text-center">
         Edit product details
       </h2>
-
-      {/* ✅ async server component */}
-      <EditForm id={params.id} />
+      <Suspense
+        fallback={
+          <div className="h-24 w-full flex items-center justify-center">
+            loading...
+          </div>
+        }
+      >
+        <EditForm id={resolvedParams.id} />
+      </Suspense>
     </div>
   );
 }
