1f44c7f24fc16b1f1dc747299689d7d51f0b3f12
mahedi066 <imam25349@gmail.com>
2025-07-25 22:14:02 +0600

    Resolved merge conflict in data.ts

--- src/lib/config/data.ts
+++ src/lib/config/data.ts
@@ -1,9 +1,3 @@
-//? ⚠️ WARNING: These values are **critical** to the application.
-//! Do **not** modify unless you fully understand their purpose and impact. !!
-//? These constants are used throughout the entire app and changing them without proper knowledge can break functionality.
-//
-//** */ (For developers only)
-//
 
 export const BASE_API = "http://10.10.10.55/";
 export const BASE_API_ENDPOINT = "http://10.10.10.55/api/";
