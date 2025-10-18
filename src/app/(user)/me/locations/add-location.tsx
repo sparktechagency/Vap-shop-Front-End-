4c5cda3f954f9d8c7ecc6948c66b256e94b4f7b7
Ksh Siaan <kshsiaan@gmail.com>
2025-10-18 09:47:23 +0600

    quick

--- src/app/(user)/me/locations/add-location.tsx
+++ src/app/(user)/me/locations/add-location.tsx
@@ -121,6 +121,7 @@ export default function AddLocation() {
             )}
           </div>
         )}
+        {/*  */}
         <DialogFooter>
           {selectedStore && (
             <Button
